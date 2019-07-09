c23f1468c9c83d314095e8f18c34e5c821eba8d7
ljbyb <ljbyb@163.com>
2019-07-09 19:12:08 +0800

    add src and build file

---
+++ dist/precache-manifest.524f08192df68f73f23eb9da96646205.js
@@ -0,0 +1,222 @@
+self.__precacheManifest = [
+  {
+    "revision": "ffa7fea70a4afab2c533",
+    "url": "/static/js/about.8e9328d9.js"
+  },
+  {
+    "revision": "eab8a7d8abd27c3c1eff",
+    "url": "/static/js/app.ea1c9a43.js"
+  },
+  {
+    "revision": "b385700c719c6c273bbc",
+    "url": "/static/css/chunk-04c4cf3f.92005c1f.css"
+  },
+  {
+    "revision": "b385700c719c6c273bbc",
+    "url": "/static/js/chunk-04c4cf3f.3f051fee.js"
+  },
+  {
+    "revision": "ecb20a6337627cbb9b3a",
+    "url": "/static/css/chunk-2666ef1c.e5f351c8.css"
+  },
+  {
+    "revision": "ecb20a6337627cbb9b3a",
+    "url": "/static/js/chunk-2666ef1c.a8aba989.js"
+  },
+  {
+    "revision": "d3b43ea14b1a50cdac3d",
+    "url": "/static/css/chunk-29086b35.e82c63f6.css"
+  },
+  {
+    "revision": "d3b43ea14b1a50cdac3d",
+    "url": "/static/js/chunk-29086b35.13c454f2.js"
+  },
+  {
+    "revision": "32aeefe5a7994c663592",
+    "url": "/static/css/chunk-3d10711d.24a30955.css"
+  },
+  {
+    "revision": "32aeefe5a7994c663592",
+    "url": "/static/js/chunk-3d10711d.4a414141.js"
+  },
+  {
+    "revision": "0401bc8191a0c6d4473c",
+    "url": "/static/css/chunk-41e6c774.f4e8c8fa.css"
+  },
+  {
+    "revision": "0401bc8191a0c6d4473c",
+    "url": "/static/js/chunk-41e6c774.da9f3094.js"
+  },
+  {
+    "revision": "1ca85186130827559cfb",
+    "url": "/static/css/chunk-4ea95ffa.d4bd1de1.css"
+  },
+  {
+    "revision": "1ca85186130827559cfb",
+    "url": "/static/js/chunk-4ea95ffa.d407b386.js"
+  },
+  {
+    "revision": "2871667343428bca6dc0",
+    "url": "/static/css/chunk-809d81fa.a0885104.css"
+  },
+  {
+    "revision": "2871667343428bca6dc0",
+    "url": "/static/js/chunk-809d81fa.2f2a29bf.js"
+  },
+  {
+    "revision": "c0b0c60dd9a57b606888",
+    "url": "/static/js/chunk-b10544a6.2f135a58.js"
+  },
+  {
+    "revision": "fa33657633d450674d36",
+    "url": "/static/css/chunk-e5c837f8.178324e1.css"
+  },
+  {
+    "revision": "fa33657633d450674d36",
+    "url": "/static/js/chunk-e5c837f8.0fee93ce.js"
+  },
+  {
+    "revision": "93b55afa7797cc6a8780",
+    "url": "/static/css/chunk-vendors.804f86b9.css"
+  },
+  {
+    "revision": "93b55afa7797cc6a8780",
+    "url": "/static/js/chunk-vendors.54318e38.js"
+  },
+  {
+    "revision": "3538ccc8c7bbc1a006b9459372d436a8",
+    "url": "/static/img/大金字塔内部中空.3538ccc8.png"
+  },
+  {
+    "revision": "44339d87de2f442974f35216108a9cf6",
+    "url": "/static/img/武王伐纣.44339d87.jpeg"
+  },
+  {
+    "revision": "08ebdb6872e8267c8533a698f4fea34b",
+    "url": "/static/img/中国人民共和国开国大典.08ebdb68.jpg"
+  },
+  {
+    "revision": "3c49aa0b3b31b3eb7ff4b7e100cb4a1b",
+    "url": "/static/img/Wechat.3c49aa0b.png"
+  },
+  {
+    "revision": "0d42a75af2fa06b812faa8139b6def7d",
+    "url": "/static/img/战国.0d42a75a.jpg"
+  },
+  {
+    "revision": "f608c54691624d422bcaab00d996f3e9",
+    "url": "/static/img/old-stone1.f608c546.jpg"
+  },
+  {
+    "revision": "91afbfa7819325b9866c6956ad36830a",
+    "url": "/static/img/孙中山.91afbfa7.jpg"
+  },
+  {
+    "revision": "fdd1bbd91c067286619da66e3e5906b5",
+    "url": "/static/img/商纣王.fdd1bbd9.png"
+  },
+  {
+    "revision": "ee164880ea9a4dd6e8df30fc9519aa4e",
+    "url": "/static/img/来自伊拉克失落之城的巨量石板.ee164880.png"
+  },
+  {
+    "revision": "d5694d1e151e00952defa0bb2138c563",
+    "url": "/static/img/book.d5694d1e.jpg"
+  },
+  {
+    "revision": "84302fe2740a9f63665f58370e7f2bbd",
+    "url": "/static/img/清-王国来朝.84302fe2.jpg"
+  },
+  {
+    "revision": "8a4c31b05a5face7a23f13bb44d0842a",
+    "url": "/static/img/元朝1.8a4c31b0.jpeg"
+  },
+  {
+    "revision": "447c6d374b7a17a5bb730b457aeae9df",
+    "url": "/static/img/秦国统一六国.447c6d37.jpg"
+  },
+  {
+    "revision": "1a98695a3ba59ddce535494c147db8f7",
+    "url": "/static/img/刘备.1a98695a.jpeg"
+  },
+  {
+    "revision": "20e5bc36f34bef2d3a010f8e1e71bc84",
+    "url": "/static/img/春秋2.20e5bc36.jpeg"
+  },
+  {
+    "revision": "ec3dc3fbf2f8955d08fe3223597f6c39",
+    "url": "/static/img/三皇五帝.ec3dc3fb.jpeg"
+  },
+  {
+    "revision": "c857095b595a94ce0ec50a7441057c1f",
+    "url": "/static/img/胡夫莎草纸.c857095b.png"
+  },
+  {
+    "revision": "82a9b4831ed403fa62017938ab70990a",
+    "url": "/static/img/西汉卫青.82a9b483.jpeg"
+  },
+  {
+    "revision": "d6d985cdfbf4d794adc7cc5f2ea32b2b",
+    "url": "/static/img/egypt2.d6d985cd.jpg"
+  },
+  {
+    "revision": "f763a21105dd63338d698e985e80121b",
+    "url": "/static/img/sina.f763a211.png"
+  },
+  {
+    "revision": "263484a323e442287f5a93b14d180937",
+    "url": "/static/img/夏朝.263484a3.jpeg"
+  },
+  {
+    "revision": "a1af610c6f43b9ba5d4506fb8f10a383",
+    "url": "/static/img/greatwall2.a1af610c.jpg"
+  },
+  {
+    "revision": "b16509df1d817cb158c1a78a40fa5e20",
+    "url": "/static/img/秦始皇兵马俑.b16509df.jpeg"
+  },
+  {
+    "revision": "ea4bb73bd275b28ea6fd8cd0d913443d",
+    "url": "/static/img/qq.ea4bb73b.png"
+  },
+  {
+    "revision": "5d651f5328375b5feb1bc8867ea16510",
+    "url": "/static/img/昆兰附近的一个新洞穴.5d651f53.png"
+  },
+  {
+    "revision": "9b81649eda09a56902494508d164a53f",
+    "url": "/static/img/new-stone1.9b81649e.jpg"
+  },
+  {
+    "revision": "34ae86aba044bdd483babc34e9f9073f",
+    "url": "/static/img/曹操.34ae86ab.jpeg"
+  },
+  {
+    "revision": "82a7c2790e078dcd5b9b78dccdb41849",
+    "url": "/static/img/复活节彩蛋.82a7c279.png"
+  },
+  {
+    "revision": "7e56f77798bda146e45ae11fce6faf8b",
+    "url": "/static/img/中亚起源说.7e56f777.jpg"
+  },
+  {
+    "revision": "2d6e36c3674e67fe0cd77fe07c7831b8",
+    "url": "/static/img/greece4.2d6e36c3.jpg"
+  },
+  {
+    "revision": "2c4515a6f5a4c2869a8152ea99d7bdd8",
+    "url": "/static/img/roma.2c4515a6.jpg"
+  },
+  {
+    "revision": "e02c6482b05da92b9f7234677a9c98c5",
+    "url": "/static/img/孙权2.e02c6482.jpeg"
+  },
+  {
+    "revision": "caf4124736d95d2056dfde0783aa88f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
